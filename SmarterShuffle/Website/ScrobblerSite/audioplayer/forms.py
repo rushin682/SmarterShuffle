efbbca2e75c79ef164eeb886f1e26dad7d2e964d
rushin682 <rushin.g@somaiya.edu>
2018-03-13 22:55:26 +0530

    Tried Youtube API json parser. Didn't work

--- SmarterShuffle/Website/ScrobblerSite/audioplayer/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-
-class SearchBar(forms.Form):
-    class Media:
-        css = {
-                'all' : ('/css/style.css'),
-            }
-        js = ('/js/jquery-2.1.4.js',)
-    search_ID = forms.CharField(label = 'Search', max_length = 100)
--- SmarterShuffle/Website/ScrobblerSite/audioplayer/urls.py
+++ SmarterShuffle/Website/ScrobblerSite/audioplayer/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [url(r'^index$', views.index, name='index'),
                url(r'^play$',views.play,name='play'),
                url('^login_complete/$', views.login_complete, name='lastfm_login_complete'),
-               url(r'^logout$',views.logout,name='logout')]
+               url(r'^logout$',views.logout,name='logout'),
+               url(r'^search_query', views.youtubeurl, name = 'youTube_query'),
+              ]
